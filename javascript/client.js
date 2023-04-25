4852c73b6b74707ce818daf294b2f61cc6462085
FrankCa99 <131309243+FrankCa99@users.noreply.github.com>
2023-04-24 23:07:22 -0400

    Username Enhanced

--- javascript/client.js
+++ javascript/client.js
@@ -1,16 +1,20 @@
-// let user
-// let validator
-// let submit_button
-// window.onload = function() {
-//     // submit_button = document.getElementById("submit_button");
-//     alert(0);
-//     // submit_button.onclick = function() {
-//     //     alert(0)
-//     //     // var name = document.getElementById("name").value;
-//     //     // var lastname = document.getElementById("lastname").value;
-//     //     // var email = document.getElementById("email").value;
-//     //     // var username = document.getElementById("username").value;
-//     //     // var password = document.getElementById("password").value;
-//     //     // console.log(name, lastname, email, username, password);
-//     // }
-// }
+let user
+let validator
+let submit_button
+let user_is_valid = false
+window.onload = function() {
+    submit_button = document.getElementById("submit_button");
+    submit_button.onclick = function() {
+        var name = document.getElementById("name").value;
+        var lastname = document.getElementById("lastname").value;
+        var email = document.getElementById("email").value;
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+
+        user = new User(name, lastname, email, username, password);
+
+        validator = new DataValidator(user);
+        user_is_valid = validator.validate_user(user);
+        console.log(user);
+    }
+}
--- javascript/user.js
+++ javascript/user.js
@@ -1,9 +1,11 @@
 class User {
-    constructor(name, age, username, password) {
+    constructor(name, lastname, email, username, password) {
         this.name = name;
-        this.age = age;
+        this.lastname = lastname;
+        this.email = email;
         this.username = username;
         this.password = password;
+        console.log(this.username);
     }
     getJSONData() {
         let user_as_json = JSON.stringify(this);
